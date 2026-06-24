de994108d26cc790e5323bc81064514c74bebb97
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-24 09:59:01 -0500

    creamos nuestro registro y nuestro arreglo

---
+++ libros.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Libros{
+    string titulo;
+    string autor;
+    string editorial;
+    int anioPublicacion;
+};
+
+int main(){
+    
+
+
+
+}
