c99cb8da67347357a986090bdda9ff521defcfa6
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-30 17:42:29 -0500

    creamos variables de acumulacion y con estructura leemos los datos de la persona para luego sumar la edad y ver si es mayor de 50

--- propuesto2.cpp
+++ propuesto2.cpp
@@ -15,7 +15,29 @@ int main(){
     cin.ignore;
 
     Persona personas[1000];
-    
+
+    int mayores = 0;
+    int suma = 0;
+
+    for(int i=0; i<n; i++){
+        cout<<" Datos de la persona: "<<i+1<<endl;
+        cout<<"Nombres: ";
+        getline(cin,personas[i].nombres);
+        cout<<"DNI: ";
+        cin>>personas[i].DNI;
+        cout<<"Edad: ";
+        cin>>personas[i].edad;
+
+        cin.ignore();
+
+        suma = suma + personas[i].edad;
+
+        if(personas[i].edad > 50){
+            mayores++;
+        }
+    }
+
+
 
 
 }
