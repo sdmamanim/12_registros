0b3165f7295808d4b2e9433318169a0cf5161974
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-30 16:57:53 -0500

    ingresamos la cantidad de empleados y creamos una estrusctura para que se pueda ingresar los datos

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -8,3 +8,25 @@ struct Empleado{
     double sueldo;
 };
 
+int main(){
+    int n;
+    cout<<"Ingrese la cantidad de trbajadores: ";
+    cin>>n;
+
+    cin.ignore();
+    Empleado empleados[1000];
+
+    for(int i=0; i<n; i++){
+        cout<<"Trabajador "<<i+1<<endl;
+
+        cout<<"Nombres: ";
+        getline(cin.empleados[i].nombres);
+        cout<<"Sexo: ";
+        cin>>empleados[i].sexo;
+        cout<<"Sueldo: ";
+        cin>>empleados[i].sueldo;
+        cin.ignore();
+    }
+
+}
+
