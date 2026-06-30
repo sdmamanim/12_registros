3a84f46ae21534ae14bcaf4990e58dfd864391d4
sdmamanim <sdmamanim@unjbg.edu.pe>
2026-06-30 18:13:09 -0500

    creamos el registro con los datos y una arreglo

---
+++ propuesto3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Persona{
+    string nombres;
+    int dia;
+    int mes;
+    int anio;
+};
+int main(){
+    int n;
+    cout<<"Ingrese la cantidad de personas ";
+    cin>>n;
+    cin.ignore();
+
+    Persona personas[1000];
+    
+    
+}
